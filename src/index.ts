b608475ca126184dcf4dd77b890d3c72b8c8a74e
Jishnu <jishnunambiar03@gmail.com>
2025-01-15 21:51:47 +0100

    Initial setup: Server with routes

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Express } from 'express';
+import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -10,4 +10,42 @@ app.use(express.json());
 
 app.listen(port, () => {
 	console.log(`[server]: Server is running at http://localhost:${port}`);
+});
+
+app.get('/', (request: Request, response: Response) => {
+	response.send('Hello World!');
+});
+
+// project CRUD operations
+app.get('/projects', (request: Request, response: Response) => {
+	response.send('Display Project Details...');
+});
+
+app.put('/projects', (request: Request, response: Response) => {
+	response.send('Update Project Details...');
+});
+
+app.post('/projects', (request: Request, response: Response) => {
+	response.send('Post New Project...');
+});
+
+app.delete('/projects', (request: Request, response: Response) => {
+	response.send('Delete Project...');
+});
+
+// reports CRUD operations
+app.get('/reports', (request: Request, response: Response) => {
+	response.send('Display report Details...');
+});
+
+app.put('/reports', (request: Request, response: Response) => {
+	response.send('Update report Details...');
+});
+
+app.post('/reports', (request: Request, response: Response) => {
+	response.send('Post new Project...');
+});
+
+app.delete('/reports', (request: Request, response: Response) => {
+	response.send('Delete Report...');
 });
